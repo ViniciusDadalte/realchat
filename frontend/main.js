f0b03c21d4765de45db022f4db1978cd23a4223d
Vinicius Dadalte <viniciusdadalte745@email.com>
2025-04-22 20:46:45 -0300

    Criei o main.js e ja coloquei as funções changeChatRoom e a sendMessage, e ja comece a configurar o websocket, e ja fiz o styles.css e coloquei uma estilização bem basica, e ja linkei esses dois arquivos no index.html

---
+++ frontend/main.js
@@ -0,0 +1,28 @@
+var selectedChat = "general";
+
+function changeChatRoom() {
+  var newChat = document.getElementById("chatroom");
+  if (newChat != null && newChat.value != selectedChat) {
+    console.log(newChat)
+  }
+  return false;
+}
+
+function sendMessage() {
+  var newMessage = document.getElementById("message");
+  if (newMessage != null) {
+    console.log(newMessage)
+  }
+  return false;
+}
+
+window.onload = function () {
+  document.getElementById("chatroom-selection").onsubmit = changeChatRoom; 
+  document.getElementById("chatroom-message").onsubmit = sendMessage; 
+
+  if (window["WebSocket"]) {
+    console.log("supports websockets"); // connect to WS
+  } else {
+    alert('Browser does not support websockets')
+  }
+}
